e03754a5b8e7742c3a58d7f48ed12bd4c182b9f8
Joerg Heffner <56156750+gitgoodjhe@users.noreply.github.com>
2022-06-08 15:18:17 +0200

    s

--- lib/taskana-cdi/src/test/java/pro/taskana/common/internal/TaskanaProducersTest.java
+++ lib/taskana-cdi/src/test/java/pro/taskana/common/internal/TaskanaProducersTest.java
@@ -27,6 +27,7 @@ public class TaskanaProducersTest {
     deployment.addDependency("org.mybatis:mybatis:3.4.2");
     deployment.addDependency("org.mybatis:mybatis-cdi:1.0.0");
     deployment.addDependency("pro.taskana:taskana-core");
+    deployment.addDependency("pro.taskana:custom-checkstyle");
     deployment.addAsResource("META-INF/beans.xml");
     deployment.addAsResource("taskana.properties");
     deployment.addAsResource("project-defaults.yml");
