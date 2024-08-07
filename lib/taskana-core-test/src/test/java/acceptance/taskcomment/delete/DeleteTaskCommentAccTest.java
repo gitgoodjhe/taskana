7ecd79b779180b92303418db895353b076cc2dcc
Jörg Heffner <joerg.heffner@envite.de>
2024-08-07 16:58:11 +0200

    s

--- lib/taskana-core-test/src/test/java/acceptance/taskcomment/delete/DeleteTaskCommentAccTest.java
+++ lib/taskana-core-test/src/test/java/acceptance/taskcomment/delete/DeleteTaskCommentAccTest.java
@@ -171,7 +171,7 @@ class DeleteTaskCommentAccTest {
   void should_FailToDeleteTaskComment_When_CommentIdDoesNotExist() throws Exception {
 
     ThrowingCallable call = () -> taskService.deleteTaskComment("non existing task comment id");
-    TaskCommentNotFoundException e = catchThrowableOfType(call, TaskCommentNotFoundException.class);
+    TaskCommentNotFoundException e = catchThrowableOfType(TaskCommentNotFoundException.class,call);
     assertThat(e.getTaskCommentId()).isEqualTo("non existing task comment id");
   }
 }
